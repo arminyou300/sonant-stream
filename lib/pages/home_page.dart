ef7c9155b48193b0541fa641444c154a4ba5e5c5
arminyou300 <arminyou300@gmail.com>
2024-11-03 00:39:46 +0330

    commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:sonant_stream/constants.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-
+    return Scaffold(
+      backgroundColor: kDarkBackgroundColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: ElevatedButton(
+              onPressed: () async{
+                await FirebaseAuth.instance.signOut();
+              },
+              child: const Text(
+                'LogOut',
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
